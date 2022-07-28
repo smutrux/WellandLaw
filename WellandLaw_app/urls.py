b6bec28687e8636ac21c12da96364da6505cded7
Fahad Khan <fkhan@edgefactor.com>
2022-07-28 14:41:18 -0400

    about-us update

--- WellandLaw_app/urls.py
+++ WellandLaw_app/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('about-us/', views.aboutus, name="about-us"),
+    path('career-centre/', views.careercentre, name="career-centre"),
+    path('contact-us/', views.contactus, name="contact-us"),
+    path('cpd-events/', views.cpdevents, name="cpd-events"),
     path('', views.index, name="index"),
+    path('landing-page/', views.landingpage, name="landing-page"),
+    path('library/', views.library, name="library"),
+    path('membership-page/', views.membershippage, name="membership-page"),
+    path('practice-portals/', views.practiceportals, name="practice-portals"),
+    path('public-resourcess/', views.publicresources, name="public-resources"),
+    path('test/', views.test, name="test"),
     
 ]
--- WellandLaw_app/views.py
+++ WellandLaw_app/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'index.html')
 
+def aboutus(request):
+    return render(request, 'about-us.html')
+
+def careercentre(request):
+    return render(request, 'career-centre.html')
+
+def contactus(request):
+    return render(request, 'contact-us.html')
+
+def cpdevents(request):
+    return render(request, 'cpd-events.html')
+
+def landingpage(request):
+    return render(request, 'landing-page')
+
+def library(request):
+    return render(request, 'library.html')
+
+def membershippage(request):
+    return render(request, 'membership-page.html')
+
+def practiceportals(request):
+    return render(request, 'practice-portals.html')
+
+def publicresources(request):
+    return render(request, 'public-resources.html')
+
+def test(request):
+    return render(request, 'test.html')
+
