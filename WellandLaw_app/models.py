e3cd9e2fbb4310a635852fce0a754ac99dc3e974
athavan jesunesan <athavanjesunesan@gmail.com>
2022-08-09 11:18:22 -0400

    Models update

--- WellandLaw_app/models.py
+++ WellandLaw_app/models.py
@@ -30,6 +30,8 @@ class Event(models.Model):
     date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=500)
     image = models.ImageField(upload_to='static/images/event')
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
     def __str__(self):
         return self.title
 
